60c96147241b67823f39afe27fb7b8218375b97c
wangyinxing <wangyinxing19@gmail.com>
2015-11-14 17:36:52 +0800

    Refactoring...

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,20 @@
 (function(){
   'use strict';
-
-    var module = angular.module('app', ['ionic', 'onsen', 'config', 'cdsaDirectives', 'cdsaServices', 'cdsaFilters']);
+    var rootScope;
+    var module = angular.module('app', ['onsen', 'config', 'cdsaDirectives', 'cdsaServices', 'cdsaFilters']);
     var xmlHttp = new XMLHttpRequest();
+
+    document.addEventListener("deviceready", function() {
+      rootScope.$broadcast("device_ready");
+    }, false);
 
     module.service('gApp', function() {
         this.fp_result = {};
         this.json = {};
     });
 
-    module.run(['$rootScope', '$ionicPlatform', function($rootScope, $ionicPlatform) {
-
-      $ionicPlatform.ready(function() {
-         
-      });
+    module.run(['$rootScope', function($rootScope) {
+      rootScope = $rootScope;
     }]);
 
     module.controller('appCtrl', ['$scope', '$sce', '$http', 'gApp', function($scope, $sce, $http, gApp) {
@@ -118,104 +119,6 @@
           break;
         }
       }
-  }]);
-
-
-  /*                          */
-  // page: home.html
-  /*                          */
-  module.controller('homeCtrl', ['$scope', '$window', 'gApp', function($scope, $window, gApp) {
-    var img = 'www/img/AppHome.png',
-        url,
-        message = "The Chinese Destiny Stars Astrology App has a free Four Pillars Calculator, free Year Star reading and you can buy 'Done for You' readings. I recommend you get it",
-        subject = 'I like this app, I think you will too!';
-
-    if (ons.platform.isAndroid()) {
-      url = 'https://play.google.com/store/apps/details?id=com.fsrc.destinystars';
-    }
-    else if (ons.platform.isIOS()) {
-      url = 'https://itunes.apple.com/us/app/family-newsletter/id888043575';
-    }
-    else {  // WP
-        url = 'http://windowsphone.com/s?appid=886fa5b2-48f9-4706-ba97-086f36f95f17';
-    }
-
-    $scope.gotoPage = gApp.gotoPage;
-
-    $scope.init = function() {
-      if (typeof(cordova) != "undefined") {
-        try {
-          cordova.getAppVersion.getVersionNumber().then(function (version) {
-            $scope.$apply(function() {
-              $scope.version = version;
-            })
-          });
-        }
-        catch(e) {
-
-        }
-      }
-    };
-
-    function onSuccess() {
-      console.log('Sharing successed !');
-    }
-
-    function onError() {
-      console.log('Sharing error !');
-      window.plugins.socialsharing.share(
-        message + ' (' + url + ')',
-        subject,
-        img,
-        url
-      );
-    }
-
-    $scope.shareViaFacebook = function() {
-      if (ons.platform.isAndroid()) {
-        window.plugins.socialsharing.shareViaFacebookWithPasteMessageHint(
-          message,
-          null,
-          url,
-          null,
-          onSuccess, onError);
-      }
-      else if (ons.platform.isIOS()) {
-        window.plugins.socialsharing.shareViaFacebook(
-          null,
-          null,
-          url,
-          null,
-          onSuccess, onError);
-      }
-      else {  // WP
-          window.plugins.socialsharing.shareViaTwitter(
-            subject + ' (' + url + ')',
-            img,
-            null,
-            onSuccess, onError);
-      }
-    };
-
-    $scope.shareViaTwitter = function () {
-        window.plugins.socialsharing.shareViaTwitter(
-            subject + ' (' + url + ')',
-            img,
-            null,
-            onSuccess, onError);
-    };
-
-    $scope.shareViaEmail = function() {
-      window.plugins.socialsharing.shareViaEmail(
-        message + ' (' + url + ')',
-        'I like this app, I think you will too!',
-        null,
-        null,
-        null,
-        img,
-        onSuccess, onError);
-    };
-
   }]);
 
 
---
+++ www/js/home.js
@@ -0,0 +1,102 @@
+/*                          */
+// page: home.html
+/*                          */
+app.controller('homeCtrl', ['$scope', '$window', 'gApp', function($scope, $window, gApp) {
+  var img = 'www/img/AppHome.png',
+      url,
+      message = "The Chinese Destiny Stars Astrology App has a free Four Pillars Calculator, free Year Star reading and you can buy 'Done for You' readings. I recommend you get it",
+      subject = 'I like this app, I think you will too!';
+
+  if (ons.platform.isAndroid()) {
+    url = 'https://play.google.com/store/apps/details?id=com.fsrc.destinystars';
+  }
+  else if (ons.platform.isIOS()) {
+    url = 'https://itunes.apple.com/us/app/family-newsletter/id888043575';
+  }
+  else {  // WP
+      url = 'http://windowsphone.com/s?appid=886fa5b2-48f9-4706-ba97-086f36f95f17';
+  }
+
+  $scope.gotoPage = gApp.gotoPage;
+
+  $scope.init = function() {
+    $scope.version = '';
+
+    
+  };
+
+  $scope.$on("device_ready", function() {
+    if (typeof(cordova) != "undefined") {
+      try {
+        cordova.getAppVersion.getVersionNumber().then(function (version) {
+          $scope.$apply(function() {
+            $scope.version = version;
+          })
+        });
+      }
+      catch(e) {
+
+      }
+    }
+  });
+
+  function onSuccess() {
+    console.log('Sharing successed !');
+  }
+
+  function onError() {
+    console.log('Sharing error !');
+    window.plugins.socialsharing.share(
+      message + ' (' + url + ')',
+      subject,
+      img,
+      url
+    );
+  }
+
+  $scope.shareViaFacebook = function() {
+    if (ons.platform.isAndroid()) {
+      window.plugins.socialsharing.shareViaFacebookWithPasteMessageHint(
+        message,
+        null,
+        url,
+        null,
+        onSuccess, onError);
+    }
+    else if (ons.platform.isIOS()) {
+      window.plugins.socialsharing.shareViaFacebook(
+        null,
+        null,
+        url,
+        null,
+        onSuccess, onError);
+    }
+    else {  // WP
+        window.plugins.socialsharing.shareViaTwitter(
+          subject + ' (' + url + ')',
+          img,
+          null,
+          onSuccess, onError);
+    }
+  };
+
+  $scope.shareViaTwitter = function () {
+      window.plugins.socialsharing.shareViaTwitter(
+          subject + ' (' + url + ')',
+          img,
+          null,
+          onSuccess, onError);
+  };
+
+  $scope.shareViaEmail = function() {
+    window.plugins.socialsharing.shareViaEmail(
+      message + ' (' + url + ')',
+      'I like this app, I think you will too!',
+      null,
+      null,
+      null,
+      img,
+      onSuccess, onError);
+  };
+
+}]);
